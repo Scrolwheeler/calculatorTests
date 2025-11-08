4fc9072663cadbe02a0c5237a1c7572f8d80a9b5
Your Name <you@example.com>
2025-11-08 13:30:45 +0200

    ktkas neiet

---
+++ script.js
@@ -0,0 +1,19 @@
+const userInput = document.getElementById("userInput")
+const userInput2 = document.getElementById("userInput2")
+const result = document.getElementById("result")
+const Calculate = document.getElementById("Calculate")
+const select = document.getElementById("select")
+
+addEventListener("click", GetResult)
+
+
+function GetResult(){
+    if (select.value = "/")
+        result.innerText = userInput.value / userInput2.value;
+    if (select.value = "*")
+        result.innerText = userInput.value * userInput2.value;
+    if (select.value = "-")
+        result.innerText = userInput.value - userInput2.value;
+    if (select.value = "+")
+        result.innerText = userInput.value + userInput2.value;
+}
